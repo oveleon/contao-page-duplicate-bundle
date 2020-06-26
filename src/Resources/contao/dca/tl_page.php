623bbe8cdfd436ee0639b3916da042e6e5ab24e5
eki89 <fabian@oveleon.de>
2020-06-26 10:29:16 +0200

    Duplicate type added in allowed pages types for oveleon company bundle.

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -7,7 +7,7 @@
  */
 
 // Add palettes to tl_page
-$GLOBALS['TL_DCA']['tl_page']['palettes']['duplicate'] = '{title_legend},title,alias,type;{meta_legend},pageTitle,robots,description,serpPreview;{duplicate_legend},jumpTo;{protected_legend:hide},protected;{cache_legend:hide},includeCache;{chmod_legend:hide},includeChmod;{expert_legend:hide},cssClass,sitemap,hide,noSearch;{tabnav_legend:hide},tabindex,accesskey;{publish_legend},published,start,stop';
+$GLOBALS['TL_DCA']['tl_page']['palettes']['duplicate'] = '{title_legend},title,alias,type;{meta_legend},pageTitle,robots,description,serpPreview;{duplicate_legend},jumpTo;{protected_legend:hide},protected;{layout_legend:hide},includeLayout;{cache_legend:hide},includeCache;{chmod_legend:hide},includeChmod;{expert_legend:hide},cssClass,sitemap,hide,noSearch;{tabnav_legend:hide},tabindex,accesskey;{publish_legend},published,start,stop';
 
 $GLOBALS['TL_DCA']['tl_page']['config']['onload_callback'][] = array('tl_page_duplicate', 'makePageDuplicateMandatory');
 
